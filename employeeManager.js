634ce139a2047d14061221ccda5dae920be88ce0
who cares <mariaaramirez80@gmail.com>
2022-12-05 11:21:08 -0800

    Ask the user initial action question to figure out what they would like to do

--- employeeManager.js
+++ employeeManager.js
@@ -21,3 +21,59 @@ connection.connect(function (err) {
 console.table(
     "\n------------ EMPLOYEE TRACKER ------------\n"
 )
+
+const initialAction = async () => {
+    try {
+        let answer = await inquirer.prompt({
+            name: 'action',
+            type: 'list',
+            message: 'What would you like to do?',
+            choices: [
+                'View Employees',
+                'View Departments',
+                'View Roles',
+                'Add Employees',
+                'Add Departments',
+                'Add Roles',
+                'Update Employee Role',
+                'Exit'
+            ]
+        });
+        switch (answer.action) {
+            case 'View Employees':
+                employeeView();
+                break;
+
+            case 'View Departments':
+                departmentView();
+                break;
+
+            case 'View Roles':
+                roleView();
+                break;
+
+            case 'Add Employees':
+                employeeAdd();
+                break
+
+            case 'Add Departments':
+                departmentAdd();
+                break
+
+            case 'Add Roles':
+                roleAdd();
+                break
+
+            case 'Update Employee Role':
+                employeeUpdate();
+                break
+
+            case 'Exit':
+                connection.end();
+                break;
+        };
+    } catch (err) {
+        console.log(err);
+        initialAction();
+    };
+}
